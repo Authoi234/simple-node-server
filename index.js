346340bbeff01715716826f9879acd1759befec6
Authoi234 <jawadkhan.authoi@gmail.com>
2023-10-19 23:39:02 +0630

    users data is loaded

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send('Simple Node Server Running')
 })
 
+const users = [
+    {id: 1, name: 'Sabana', email: 'sabana@gmail.com'},
+    {id: 2, name: 'abana', email: 'abana@gmail.com'},
+    {id: 2, name: 'lorbana', email: 'lorbana@gmail.com'}
+];
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
 app.listen(port, () => {
-    console.log('Simple not surver running on port');
+    console.log(`Simple not surver running on port ${port}`);
 })
